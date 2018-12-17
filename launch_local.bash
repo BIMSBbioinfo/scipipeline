6fb2eeaeb2a0332e94e60ade30bcb82a5b57c471
Wolfgang Kopp <wolfgang.kopp@mdc-berlin.de>
2018-12-17 13:42:29 +0100

    Write cluster log to log dir

--- launch_local.bash
+++ launch_local.bash
@@ -1,3 +1,4 @@
 export OMP_NUM_THREADS=1
+LOGDIR=/scratch/AG_Akalin/wkopp/alison_sciatac_output/logs
 snakemake -s main.snake --jobs 10 --local-cores 10 \
- --cluster "qsub -v OMP_NUM_THREADS -V -l h_vmem={resources.mem_mb}m -l h_stack=10m -pe smp {threads} -N {rule}.${JOB_ID} " --reason --rerun-incomplete
+ --cluster "qsub -v OMP_NUM_THREADS -V -l h_vmem={resources.mem_mb}m -l h_stack=10m -pe smp {threads} -N {rule}.\${{JOB_ID}} -o ${LOGDIR}/rule_{rule}.o\${{JOB_ID}} -e  ${LOGDIR}/rule_{rule}.e\${{JOB_ID}} " --reason --rerun-incomplete
